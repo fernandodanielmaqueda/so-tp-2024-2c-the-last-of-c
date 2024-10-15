2efc7cbd1d22e8ec71c5396a4919826cb6fb9719
Fernando Daniel Maqueda <fmaqueda@frba.utn.edu.ar>
2024-10-15 12:49:28 -0300

    Makefile: Agrego flag --show-leak-kinds=all para memcheck

--- mkframework/main.mk
+++ mkframework/main.mk
@@ -139,7 +139,7 @@ VALGRIND_TOOLS=none memcheck helgrind
 #   Other Valgrind tools are: cachegrind , callgrind , drd , massif , dhat , lackey , exp-bbv , etc.
 # The options to pass to each respective tool, correspondingly
 VALGRIND_none_FLAGS=
-VALGRIND_memcheck_FLAGS=--leak-check=full --track-origins=yes
+VALGRIND_memcheck_FLAGS=--leak-check=full --show-leak-kinds=all --track-origins=yes
 VALGRIND_helgrind_FLAGS=
 
 # Option to output version information and exit (commmonly --version , -v or -V) in the corresponding programs
