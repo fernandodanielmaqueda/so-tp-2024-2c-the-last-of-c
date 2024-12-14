0bd10300d65fe4b57dc2cc388f7905b512c5aed6
Fernando Daniel Maqueda <fmaqueda@frba.utn.edu.ar>
2024-12-13 22:18:33 -0300

    Scripts: log_rm.sh: rm -f

--- scripts/log_rm.sh
+++ scripts/log_rm.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+printf "Eliminando archivos de log\n"
+
 set -x
 
-find . -type f -name '*.log' -exec rm {} +
+find . -type f -name '*.log' -exec rm -f {} +
 # find . -type f -name '*.log' -exec rm -i {} +
+
+{ set +x ; } 2>/dev/null
+
+printf "Archivos de log eliminados\n"
